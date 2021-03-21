e4285b0790992b3e223e17550529edfc649ef1ec
Lucas Valle <lucaspvalle@gmail.com>
2021-03-21 17:35:58 -0300

    Versão de release para usuário (necessita de correções)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <Windows.h> //MessageBoxA
-#include <chrono>
 #include "ga.h"
-#include "testes.h"
 using namespace std;
-using namespace chrono;
 
 vector<planilha> ler_planilha(float criterio_peso) {
     //leitura de arquivo CSV com inputs de fardos
@@ -70,13 +67,9 @@ void mapa(ga algoritmo) {
     arq.close();
 }
 
-relatorio sumario(int idx, int populacaoTam, int geracaoTam, double mutacaoProb, vector<planilha> inputFardos, unsigned int semente, int classes) {
+void sumario(int populacaoTam, int geracaoTam, double mutacaoProb, vector<planilha> inputFardos, unsigned int semente, int classes) {
 
     vector<double> fitval;
-    double fit_in, fit_out;
-    time_point<system_clock> comeco, fim; //cronometros
-
-    comeco = system_clock::now(); //iniciando cronometro
 
     ga algoritmo(populacaoTam, mutacaoProb, inputFardos); //inicializando o algoritmo genético
     algoritmo.seed(semente); //semente para geracao de numeros aleatorios
@@ -84,72 +77,25 @@ relatorio sumario(int idx, int populacaoTam, int geracaoTam, double mutacaoProb,
     algoritmo.init(); //inicializando a populacao para evolucao
     fitval = algoritmo.fitness(classes); //avaliando a populacao inicializada
 
-    fit_in = *max_element(fitval.begin(), fitval.end()); //valor fitness inicial do algoritmo
-    
     for (int idx = 0; idx < geracaoTam; idx++) { //iteracao de geracoes
         fitval = algoritmo.fitness(classes);
         algoritmo.cruzamento();
         algoritmo.mutacao();
     }
 
-    fit_out = *max_element(fitval.begin(), fitval.end()); //valor fitness final do algoritmo
-
-    fim = system_clock::now(); //parando cronometro
-    duration<double> segundos = fim - comeco; //calculando tempo de execucao
-
-    cout << fit_out << " com diferenca de " << fit_out - fit_in << endl;
-
-    //mapa(algoritmo);
-    //MessageBoxA(NULL, (LPCSTR)"Algoritmo executado com sucesso!", (LPCSTR)"Disposição de Fardos", MB_ICONINFORMATION);
-    return { idx, populacaoTam, geracaoTam, mutacaoProb, fit_in, fit_out, segundos.count() };
-}
-
-void testar() {
-
-    vector<relatorio> saida;
-    vector<grupos> parametros;
-    vector<planilha> inputFardos;
-    unsigned int semente = 0, rodada = 0;
-
-    vector<double> mut = { 0.05 };
-    vector<int> pop = { 500, 600, 700, 800, 900, 1000 }, grc = { 250 };
-
-    testes Iniciador;
-    parametros = Iniciador.combinador();
-
-    for (unsigned int n = 0; n < parametros.size(); n++) {
-        for (unsigned int inst = 0; inst < 2; inst++) {
-
-            rodada++;
-            cout << "Rodada " << rodada << endl;
-
-            inputFardos = Iniciador.gerarInstancias(parametros[n].fardos, parametros[n].porcentagem, parametros[n].classes);
-
-            for (unsigned int i = 0; i < pop.size(); i++)
-                for (unsigned int j = 0; j < grc.size(); j++)
-                    for (unsigned int k = 0; k < mut.size(); k++) {
-                        semente++;
-                        saida.push_back(sumario(rodada, pop[i], grc[j], mut[k], inputFardos, semente, parametros[n].classes)); //executando o algoritmo com os parametros testes
-                    }
-        }
-    }
-    Iniciador.resultados(saida);
+    mapa(algoritmo);
+    MessageBoxA(NULL, (LPCSTR)"Algoritmo executado com sucesso!", (LPCSTR)"Disposição de Fardos", MB_ICONINFORMATION);
 }
 
 int main() {
 
-    //FreeConsole();  //fechar o prompt de comando durante a execucao
-    srand(static_cast<unsigned int>(time(NULL)));
+    FreeConsole();  //fechar o prompt de comando durante a execucao
+    vector<planilha> inputFardos;
 
-    //vector<planilha> inputFardos;
-    //double mutacaoProb = 0.1;
-    //int populacaoTam = 400, geracaoTam = 400;
-    //float criterio_peso = 220; //classificacao de tamanhos de fardos
+    inputFardos = ler_planilha(220); //leitura de planilha para input
 
-    //inputFardos = ler_planilha(criterio_peso); //leitura de planilha para input
-    //sumario(0, populacaoTam, geracaoTam, mutacaoProb, inputFardos, static_cast<double>(time(NULL)), 100);
-
-    testar();
+    int classes = inputFardos.size(); //tem que agrupar as procedências de diferentes boxes!
+    sumario(900, 250, 0.05, inputFardos, static_cast<double>(time(NULL)), classes);
 
     return 0;
 }
