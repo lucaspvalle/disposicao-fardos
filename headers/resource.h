e558b3fb96b62ee5aa905ec793f10091a62ba4b6
Lucas Valle <lucaspvalle@gmail.com>
2021-06-27 14:40:47 -0300

    Limpando repositório

--- headers/resource.h
+++ headers/resource.h
@@ -2,14 +2,11 @@
 // Arquivo de inclusão gerado pelo Microsoft Visual C++.
 // Usado por Mapa de Fardos.rc
 
-#define IDC_MYICON                      2
 #define IDD_MAPADEFARDOS_DIALOG         102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
 #define IDM_EXIT                        105
-#define IDI_MAPADEFARDOS                107
-#define IDI_SMALL                       108
 #define IDC_MAPADEFARDOS                109
 #define IDR_MAINFRAME                   128
 #define ID_ARQUIVO_ENTRADA              32774
